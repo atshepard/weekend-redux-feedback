337ae59ec4631f7a0c0c392e13b024af7a6391f7
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-04-01 15:59:58 -0500

    adds dependencies and basic task outline

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import Header from '../Header/Header'
 
 function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
+      <Header />
+
+      {/* will need minimum 5 Routes: Feeling, Understanding,  (how well are you being) Supported, Comments, Submit */}
+      {/* /feeling sends payload to reducer with integer */}
+      {/* /understanding sends payload to reducer with integer */}
+      {/* /support sends payload to reducer with integer */}
+      {/* /comments sends payload of string text */}
+      {/* /submit confirms with user, grabs state and sends post to database */}
+
+      {/* /admin gets all reflections from database sorted by date */}
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+
+    return (
+        <>
+            <header className='App-header'>
+                <h1 className='App-title'>Feedback!</h1>
+                <h4>Don't forget it!</h4>
+            </header>
+        </>
+    )
+}
+
+export default Header;
